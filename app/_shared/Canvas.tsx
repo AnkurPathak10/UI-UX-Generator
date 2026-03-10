10d9f9b4f4595aabdb833baabe0e0636e23e6a1b
Ankur <ankurpathak795@gmail.com>
2026-03-11 01:33:23 +0530

    generating UI designs using AI

---
+++ app/_shared/Canvas.tsx
@@ -0,0 +1,87 @@
+"use client"
+import React, { useState } from 'react'
+import {TransformWrapper, TransformComponent} from "react-zoom-pan-pinch"
+import ScreenFrame from './ScreenFrame'
+import { ProjectType, ScreenConfig } from '@/data/types'
+import { Skeleton } from '@/components/ui/skeleton'
+import { Rnd } from 'react-rnd'
+
+type Props = {
+    projectDetail: ProjectType | undefined,
+    screenConfig: ScreenConfig[],
+    loading?: boolean,
+}
+
+function Canvas({projectDetail, screenConfig, loading}: Props) {
+    const [panningEnabled, setPanningEnabled] = useState(true);
+
+    const isMobile = projectDetail?.device == 'mobile';
+
+    const SCREEN_WIDTH = isMobile ? 400 : 1200;
+    const GAP = isMobile ? 10 : 70;
+    const SCREEN_HEIGHT = 800;
+
+
+  return (
+    <div className='w-full h-screen bg-gray-100'
+        style={{
+            backgroundImage: "radial-gradient(rgba(0,0,0,0.15) 1px, transparent 1px)",
+            backgroundSize: "20px 20px"
+        }}    
+    >
+        
+        <TransformWrapper 
+            initialScale={0.7} 
+            minScale={0.2}
+            maxScale={3}
+            initialPositionX={50} 
+            initialPositionY={50}
+            limitToBounds={false}
+            wheel={{step : 0.8}}        //how fast you want to zoom-in/zoom-out with mouse scroller
+            doubleClick={{disabled: false}}
+            panning={{disabled: !panningEnabled}}
+        >
+            <TransformComponent
+                wrapperStyle={{width: '100%', height: '100%'}} 
+            >
+                
+                {screenConfig?.map((screen, index) => (
+                    screen?.code ? (
+                        <ScreenFrame
+                            key={index}
+                            x={index * (SCREEN_WIDTH + GAP)}
+                            y={0}
+                            width={SCREEN_WIDTH}
+                            height={SCREEN_HEIGHT}
+                            setPanningEnabled={setPanningEnabled}
+                            htmlCode={screen?.code}
+                            projectDetail={projectDetail}
+                        />
+                    ) : (
+                        <Rnd
+                            key={index}
+                            default={{ x: index * (SCREEN_WIDTH + GAP), y: 0, width: SCREEN_WIDTH, height: SCREEN_HEIGHT }}
+                            disableDragging
+                            enableResizing={false}
+                        >
+                            <div className='bg-white flex flex-col gap-4 p-5 rounded-2xl w-full h-full'>
+                                <Skeleton className='w-full rounded-lg h-10 bg-gray-200'/>
+                                <Skeleton className='w-[50%] rounded-lg h-20 bg-gray-200'/>
+                                <Skeleton className='w-[70%] rounded-lg h-10 bg-gray-200'/>
+                                <Skeleton className='w-[30%] rounded-lg h-10 bg-gray-200'/>
+                                <Skeleton className='w-full rounded-lg h-10 bg-gray-200'/>
+                                <Skeleton className='w-[50%] rounded-lg h-20 bg-gray-200'/>
+                                <Skeleton className='w-[70%] rounded-lg h-10 bg-gray-200'/>
+                                <Skeleton className='w-[30%] rounded-lg h-10 bg-gray-200'/>
+                            </div>
+                        </Rnd>
+                    )
+                ))}
+            </TransformComponent>
+            
+        </TransformWrapper>
+    </div>
+  )
+}
+
+export default Canvas
---
+++ app/_shared/ScreenFrame.tsx
@@ -0,0 +1,73 @@
+import { themeToCssVars } from '@/data/themes';
+import { ProjectType } from '@/data/types';
+import { GripVertical } from 'lucide-react';
+import React, { useState } from 'react'
+import {Rnd} from "react-rnd";
+
+type Props = {
+    x: number,
+    y: number,
+    setPanningEnabled: (enabled: boolean) => void
+    width: number,
+    height: number,
+    htmlCode: string | undefined,
+    projectDetail: ProjectType | undefined,
+}
+function ScreenFrame({x, y, setPanningEnabled, width, projectDetail, height, htmlCode}: Props) {
+
+    const html = `
+        <!doctype html>
+        <html>
+            <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <!-- Google Font -->
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+
+
+            <!-- Tailwind + Iconify -->
+            <script src="https://cdn.tailwindcss.com"></script>
+            <script src="https://code.iconify.design/iconify-icon/3.0.0/iconify-icon.min.js"></script>
+            <style >
+                ${themeToCssVars(projectDetail?.theme)}
+            </style>
+            </head>
+        <body class="bg-background text-foreground w-full">
+        ${htmlCode ?? ""}
+        </body>
+        </html>
+    `;
+  
+  return (
+    <Rnd default={{
+            x,
+            y,
+            width: width,
+            height: height,
+        }}
+        dragHandleClassName='drag-handle'
+        enableResizing={{
+            bottomLeft: true,
+            bottomRight: true,
+        }}
+        onDragStart={()=>setPanningEnabled(false)}
+        onDragStop={()=>setPanningEnabled(true)}
+        onResize={()=>setPanningEnabled(false)}
+        onResizeStop={()=>setPanningEnabled(true)}
+    >
+        <div className='drag-handle flex gap-2 items-center cursor-move bg-white rounded-lg p-5'>
+            <GripVertical className='text-grey-500 h-4 w-4'/> Drag Here
+        </div>
+        <iframe
+            className='w-full h-[calc(100%-40px)] rounded-2xl mt-5 bg-white'
+            sandbox='allow-same-origin allow-scripts'
+            srcDoc={html}
+        />
+    </Rnd>
+  )
+}
+
+export default ScreenFrame
